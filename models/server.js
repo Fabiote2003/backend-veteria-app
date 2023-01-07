5751d50878b18344388442684177a0683f1acf09
Fabio <fabiote2003@gmail.com>
2023-01-07 16:32:16 -0300

    first commit

---
+++ models/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+
+    }
+
+    routes() {
+
+    }
+
+    middlewares() {
+        
+    }
+
+}
+
+module.exports = Server
